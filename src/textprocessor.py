793ea2df0485a8673f8e2d4c064c8b35481b28c0
Nart <daniel.abzakh@gmail.com>
2026-01-29 13:22:29 +0300

    updated

--- src/textprocessor.py
+++ src/textprocessor.py
@@ -100,6 +100,7 @@ class TextProcessor:
 
             # IPA Prosodic Punctuation
             # '.': ' ‖', ',': ' |', '?': ' ↗', '!': ' ↘', ' ': ' '
+            '…':'.'
         }
 
         output = []
